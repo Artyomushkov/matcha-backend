3881815a37b50a14c39084a529e387a4dfa7d1e0
Artem Ushkov <aushkov@griddynamics.com>
2024-11-27 10:51:28 +0400

    test with script

--- script.sh
+++ script.sh
@@ -1,10 +1,3 @@
-#!/bin/bash
-
-# Navigate to the project directory
 cd /home/jamatchaserver/matcha-backend
-
-# Pull the latest code from the repository
 git pull
-
-# Rebuild and recreate the Docker service
 docker-compose up -d --build --force-recreate matcha
